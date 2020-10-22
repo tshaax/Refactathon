87e925f9cdee4c212a59902dbc17205d96add95f
Hendrik Jacobs <HendrikJ@Osiristrading.com>
2020-10-22 15:24:04 +0200

    Empty Code Check-In

---
+++ Readme.md
@@ -0,0 +1,23 @@
+# Introduction
+This project contains a raw implementation of [Conway's Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life).
+The code only has the program.cs file with a single function.
+No forethought has gone into the design or structure of the code.
+
+# Your Mission
+If you choose to accept, refactor this code to the n-th degree, till you're happy or until time runs out.
+
+## Instructions
+1. clone the master branch.
+2. create a new branch for your squad (you won't be able to edit the master branch).
+3. refactor
+4. Let us know when you're done
+5. The repository will be locked after the deadline is reached, so check in regularly
+ 
+ ###The deadline for this task is 2020-10-09 18:00
+
+# Tips
+1. It's easy to test when using a simple deterministic pattern.
+2. Martin Fowler wrote a book about refactoring.
+3. It can help if only one person codes at any time see: Pair-Programming.
+
+#Enjoy. Good luck. May the best squad win.!
---
+++ c#/Refactoring.Conway/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace Refactoring.Conway
+{
+    class Program
+    {
+        //TODO: Refactor this
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+            void OnCancelKeyPress(object sender, ConsoleCancelEventArgs args)
+            {
+                args.Cancel = true;
+                cancellationTokenSource.Cancel();
+                cancellationTokenSource.Token.ThrowIfCancellationRequested();
+            }
+            Console.CancelKeyPress += OnCancelKeyPress;
+
+            try
+            {
+
+                //CODE GOES HERE
+                while(!cancellationTokenSource.IsCancellationRequested)
+                {
+                    Console.In.ReadLine();
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                //DO NOTHING
+            }
+            Console.CancelKeyPress -= OnCancelKeyPress;
+        }
+    }
+}
