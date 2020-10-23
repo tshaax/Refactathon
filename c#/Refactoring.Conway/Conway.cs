c4919e2c4a67183ae5dcf66a2283a9deeb74028c
Hendrik Jacobs <hendrik.jacobs.927@gmail.com>
2020-10-23 15:34:55 +0200

    final-commit

---
+++ c#/Refactoring.Conway/Conway.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Refactoring.Conway
+{
+    public class Conway
+    {
+        public Conway(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.Board = new bool[this.Width, this.Height];
+        }
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public bool[,] Board { get; set; }
+
+        public bool[,] GetNewBoard()
+        {
+            bool[,] newBoard = new bool[this.Width, this.Height];
+
+            for (var x = 0; x < this.Width; x++)
+            {
+                for (var y = 0; y < this.Height; y++)
+                {
+                    var livingNeighbourCount = 0;
+
+                    for (var xScan = x - 1; xScan < x + 2; xScan++)
+                    {
+                        if (xScan < 0 || xScan >= this.Width)
+                        {
+                            continue;
+                        }
+
+                        for (var yScan = y - 1; yScan < y + 2; yScan++)
+                        {
+                            if (xScan == x && yScan == y)
+                            {
+                                continue;
+                            }
+                            if (yScan >= 0 && yScan < this.Height && Board[xScan, yScan])
+                            {
+                                livingNeighbourCount += 1;
+                            }
+                        }
+                    }
+                    newBoard[x, y] = (Board[x, y] && livingNeighbourCount == 2) || livingNeighbourCount == 3;
+                }
+            }
+            return newBoard;
+        }
+
+        public bool[,] GetBoard()
+        {
+            var total = (this.Width * this.Height);
+            var ratio = (total * 40) / 100;
+
+            for (var x = 0; x < this.Width; x++)
+            {
+                for (var y = 0; y < this.Height; y++)
+                {
+                    Board[x, y] = RandomNumberGenerator.GetInt32(0, total) < ratio;
+                }
+            }
+            return Board;
+        }
+
+        public bool SocietyDied()
+        {
+            var societyDied = true;
+            var boardLength = this.Board.GetLength(1) - 1;
+
+            for (var x = 0; x < this.Width; x++)
+            {
+                for (var y = 0; y < this.Height; y++)
+                {
+                    if (this.Board[x, y])
+                    {
+                        Console.Write("0");
+                        societyDied = false;
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+                Console.WriteLine();
+            }
+            return societyDied;
+        }
+    }
+}
--- c#/Refactoring.Conway/Program.cs
+++ c#/Refactoring.Conway/Program.cs
@@ -1,136 +1,114 @@
 using System;
-using System.Security.Cryptography;
 using System.Threading;
 
 namespace Refactoring.Conway
 {
     class Program
     {
-        //TODO: Refactor this
+        static CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
         static void Main(string[] args)
         {
-            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-
-            void OnCancelKeyPress(object sender, ConsoleCancelEventArgs args)
-            {
-                args.Cancel = true;
-                cancellationTokenSource.Cancel();
-            }
             Console.CancelKeyPress += OnCancelKeyPress;
 
             try
             {
-                int width;
-                string inputWidth;
-                do
-                {
-                    Console.WriteLine("What is the width of the board?");
-                    inputWidth = Console.ReadLine();
-                }
-                while (!int.TryParse(inputWidth, out width));
+                var width = InputWidth();
+                var height =InputHeight();
+                var generations = InputGenerations();
 
-                int height;
-                string inputHeight;
-                do
-                {
-                    Console.WriteLine("What is the height of the board?");
-                    inputHeight = Console.ReadLine();
-                }
-                while (!int.TryParse(inputHeight, out height));
+                Conway conway = new Conway(width, height);
 
-                bool[,] board = new bool[width, height];
-                int total = (width * height);
-                int ratio = (total * 40) / 100;
-
-                for (int x = 0; x < width; x++)
-                {
-                    for (int y = 0; y < height; y++)
-                    {
-                        board[x, y] = RandomNumberGenerator.GetInt32(0, total) < ratio;
-                    }
-                }
-
-                int generations;
-                string inputGenerations;
-                do
-                {
-                    Console.WriteLine("How many generations does the board run for");
-                    inputGenerations = Console.ReadLine();
-                }
-                while (!int.TryParse(inputGenerations, out generations));
+                conway.Board = conway.GetBoard();
+                
                 int i;
                 for (i = 0; i <= generations && !cancellationTokenSource.IsCancellationRequested; i++)
                 {
-                    bool societyDied = true;
-
                     Console.Clear();
-                    Console.WriteLine($"Generation: {i}");
-                    for (int x = 0; x < width; x++)
-                    {
-                        for (int y = 0; y < height; y++)
-                        {
-                            if (board[x, y])
-                            {
-                                Console.Write("0");
-                                societyDied = false;
-                            }
-                            else
-                            {
-                                Console.Write(".");
-                            }
-                            if (y == board.GetLength(dimension: 1) - 1)
-                            {
-                                Console.WriteLine();
-                            }
-                        }
-                    }
 
-                    if(societyDied)
+                    Console.WriteLine($"Generation: {i}");
+
+                    bool societyDied = conway.SocietyDied();
+
+                    if (societyDied)
                     {
                         Console.WriteLine("I guess that's the end of our little society.");
+
                         break;
                     }
 
-                    bool[,] newBoard = new bool[width, height];
-                    for (int x = 0; x < width; x++)
-                    {
-                        for (int y = 0; y < height; y++)
-                        {
-                            int livingNeighbourCount = 0;
-                            for (int xScan = x - 1; xScan < x + 2; xScan++)
-                            {
-                                if (xScan < 0 || xScan >= width)
-                                {
-                                    continue;
-                                }
-                                for (int yScan = y - 1; yScan < y + 2; yScan++)
-                                {
-                                    if (xScan == x && yScan == y)
-                                    {
-                                        continue;
-                                    }
+                    conway.Board = conway.GetNewBoard();
 
-                                    if (yScan >= 0 && yScan < width && board[xScan, yScan])
-                                    {
-                                        livingNeighbourCount += 1;
-                                    }
-                                }
-                            }
-                            newBoard[x, y] = (board[x, y] && livingNeighbourCount == 2) || livingNeighbourCount == 3;
-                        }
-                    }
-
-                    board = newBoard;
-                    Thread.Sleep(TimeSpan.FromSeconds(value: 1));
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
                 }
+
                 Console.WriteLine($"Generation: {i} - Output Completed! Press any key to exit.");
+
                 Console.ReadKey();
             }
             catch (OperationCanceledException)
             {
                 //DO NOTHING
             }
+
             Console.CancelKeyPress -= OnCancelKeyPress;
         }
+
+        
+
+        private static int InputHeight()
+        {
+            string inputHeight;
+            int height;
+            do
+            {
+                Console.WriteLine("What is the height of the board?");
+                inputHeight = Console.ReadLine();
+
+            } while (!int.TryParse(inputHeight, out height));
+
+            return height;
+        }
+
+        private static int InputWidth()
+        {
+            string inputWidth;
+            int width;
+
+            do
+            {
+                Console.WriteLine("What is the width of the board?");
+                inputWidth = Console.ReadLine();
+
+            } while (!int.TryParse(inputWidth, out width));
+
+            return width;
+        }
+
+        private static int InputGenerations()
+        {
+            string inputGenerations;
+            int generations;
+
+            do
+            {
+                Console.WriteLine("How many generations does the board run for");
+                inputGenerations = Console.ReadLine();
+
+            } while (!int.TryParse(inputGenerations, out generations));
+
+            return generations;
+
+        }
+
+
+        #region Events
+        private static void OnCancelKeyPress(object sender, ConsoleCancelEventArgs args)
+        {
+            args.Cancel = true;
+
+            cancellationTokenSource.Cancel();
+        }
+        #endregion
     }
 }
