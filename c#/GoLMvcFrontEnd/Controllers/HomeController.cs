7f1576b5fb6de1dc7fdbdcc77eec8b39578545a9
David van Rensburg <davidvr@ragingriver.co.za>
2020-10-23 14:58:14 +0200

    Rex Changes

--- c#/GoLMvcFrontEnd/Controllers/HomeController.cs
+++ c#/GoLMvcFrontEnd/Controllers/HomeController.cs
@@ -12,6 +12,8 @@ using GoLMvcFrontEnd.Models;
 using GameOfLife;
 using Microsoft.Extensions.Configuration;
 
+
+
 namespace GoLMvcFrontEnd.Controllers
 {
     public class HomeController : Controller
@@ -19,21 +21,29 @@ namespace GoLMvcFrontEnd.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IConfiguration _configuration;
 
+
+
         public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
             _configuration = configuration;
         }
 
+
+
         public IActionResult Index()
         {
             return View();
         }
 
+
+
         public IActionResult Privacy()
         {
             return View();
         }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
@@ -41,15 +51,17 @@ namespace GoLMvcFrontEnd.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public async Task<IActionResult> GetGameOfLife(int width, int height, int generations)
+
+
+        public async Task<JsonResult> GetGameOfLife(int width, int height, int generations)
         {
-            using var handler = new HttpClientHandler{ServerCertificateCustomValidationCallback = (message, certificate2, arg3, arg4) => true};
-            using var client = new HttpClient(handler){BaseAddress = new Uri(_configuration.GetValue<string>("LocalBaseUrl")) };
+            using var handler = new HttpClientHandler { ServerCertificateCustomValidationCallback = (message, certificate2, arg3, arg4) => true };
+            using var client = new HttpClient(handler) { BaseAddress = new Uri(_configuration.GetValue<string>("LocalBaseUrl")) };
             var request =
                 await client.GetAsync(
                     $"api/GameOfLifeData/GetAsStrings?width={width}&height={height}&generations={generations}");
             var result = JsonSerializer.Deserialize<List<string>>(await request.Content.ReadAsStringAsync());
-            return Ok(result);
+            return new JsonResult(result);
         }
     }
 }
--- c#/GoLMvcFrontEnd/Startup.cs
+++ c#/GoLMvcFrontEnd/Startup.cs
@@ -50,7 +50,7 @@ namespace GoLMvcFrontEnd
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=GetGameOfLife}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
